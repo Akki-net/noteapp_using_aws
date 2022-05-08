26c33a48c7e881650a4d38abb8719d839339d1f8
unknown <akashsharma.zak@gmail.com>
2022-05-08 21:10:16 +0530

    creating notes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,125 @@
-import React, {useEffect, useReducer} from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { API } from 'aws-amplify'
-import { List } from 'antd'
+import { List, Input, Button } from 'antd'
 import 'antd/dist/antd.css'
 import { listNotes } from './graphql/queries'
+import { v4 as uuid } from 'uuid'
+import { createNote as CreateNote } from './graphql/mutations'
+
+const CLIENT_ID = uuid()
 
 const initialState = {
   notes: [],
   loading: true,
   error: false,
   form: { name: '', description: '' }
- }
+}
 
- function reducer(state, action) {
-  switch(action.type) {
-  case 'SET_NOTES':
-  return { ...state, notes: action.notes, loading: false }
-  case 'ERROR':
-  return { ...state, loading: false, error: true }
-  default:
-  return state
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] }
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
+    default:
+      return state
   }
- } 
+}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   async function fetchNotes() {
     try {
-    const notesData = await API.graphql({
-    query: listNotes
-    })
-    dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+      const notesData = await API.graphql({
+        query: listNotes
+      })
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
     } catch (err) {
-    console.log('error: ', err)
-    dispatch({ type: 'ERROR' })
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
     }
-   }
+  }
 
-   useEffect(() => {
+  useEffect(() => {
     fetchNotes()
-   }, [])
+  }, [])
 
-   function renderItem(item) {
+  function renderItem(item) {
     return (
-    <List.Item style={styles.item}>
-    <List.Item.Meta
-    title={item.name}
-    description={item.description}
-    />
-    </List.Item>
+      <List.Item style={styles.item}>
+        <List.Item.Meta
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
     )
-   }
+  }
 
-   const styles = {
-    container: {padding: 20},
-    input: {marginBottom: 10},
+  const styles = {
+    container: { padding: 20 },
+    input: { marginBottom: 10 },
     item: { textAlign: 'left' },
     p: { color: '#1890ff' }
-   }   
-   
-   return (
+  }
+
+  async function createNote() {
+    const { form } = state
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description')
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note!')
+    } catch (err) {
+      console.log("error: ", err)
+    }
+  }
+
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  }
+
+  return (
     <div style={styles.container}>
-    <List
-    loading={state.loading}
-    dataSource={state.notes}
-    renderItem={renderItem}
-    />
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >Create Note</Button>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
-   )
-   
+  )
+
 }
 
 export default App;
